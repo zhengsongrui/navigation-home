8076d67bd86b1804fdbf8ae1691d598393343144
zheongsongrui <1245881972@qq.com>
2026-08-04 11:40:40 +0800

    修改id

--- server/config.js
+++ server/config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
    
     {
-      id: 3,
+      id: 2,
       name: "字体压缩工具",
       description: "用于web前端的otf字体压缩转换为woff2字体工具，可自定义压缩字符集。",
       url: "http://zhengsongrui.life/frp/font-compression",
@@ -19,7 +19,7 @@ module.exports = {
       writeStatus: "已完成",
     },
     {
-      id: 4,
+      id: 3,
       name: "live2d智能虚拟助手",
       description: "使用kotlin开发，基于Live2D的虚拟助手，后端采用noedjs，接入了语音识别、语音合成、在线/本地AI模型。",
       // url: "http://zhengsongrui.life/frp/font-compression",
@@ -27,7 +27,7 @@ module.exports = {
       writeStatus: "开发中",
     },
     {
-      id: 5,
+      id:4,
       name: "远程开机助手",
       description: "用于远程开启电脑的工具，服务端基于nodejs开发，app端使用kotlin开发，仅支持PCIE网络唤醒的主板。",
       // url: "http://zhengsongrui.life/frp/font-compression",
@@ -35,7 +35,7 @@ module.exports = {
       writeStatus: "已完成",
     },
      {
-      id: 2,
+      id: 5,
       name: "个人博客v3",
       description:
         "用当前主流AI开发流程开发的博客，由开发者制定架构、规范和规则，AI辅助实现。",
